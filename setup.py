1eb3a46a042f4d1485b106fd16c635fc9bd1329a
Niels Martin Hansen <niels@agigen.se>
2013-10-04 17:30:11 +0200

    Distutils installation script and related files
    Closes #1

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#encoding=utf8
+
+from distutils.core import setup
+
+with open('README') as file:
+    long_description = file.read()
+
+setup(
+    name='billogram_api',
+    version='1.0',
+    author='Billogram AB',
+    author_email='support@billogra.com',
+    description='Library for connecting to the Billogram v2 API',
+    long_description=long_description,
+    url='https://billogram.com/api/documentation',
+    license='MIT',
+    py_modules=['billogram_api'],
+)
