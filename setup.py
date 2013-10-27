a1c4d41c8e9dcc98ae13c32c81a6a994ba360eac
Andreas Pelme <andreas@pelme.se>
2013-10-27 10:41:35 +0100

    Fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name='billogram_api',
     version='1.0',
     author='Billogram AB',
-    author_email='support@billogra.com',
+    author_email='support@billogram.com',
     description='Library for connecting to the Billogram v2 API',
     long_description=long_description,
     url='https://billogram.com/api/documentation',
